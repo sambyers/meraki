ee1360ecc5228dc398b083c48c57b3675a8c63a7
Sam Byers <sam@sambyers.net>
2018-05-18 16:50:24 -0400

    added update_vlans

--- enterprise_vlan_configuration.py
+++ enterprise_vlan_configuration.py
@@ -1,13 +1,13 @@
 # VLAN names and subet configuration
 enterprise_networks = [
     {
-        'network_name': 'Home-Moseley-VA',
+        'network_name': 'Lab-test-hub1',
         'VLANS': [
             {'id': 128, 'name': 'Default', "applianceIp": "192.168.128.1", 'subnet': '192.168.128.0/24', "fixedIpAssignments": {},  "reservedIpRanges": [], "dnsNameservers": "opendns"}
         ]
     },
     {
-        'network_name': 'Home-Moseley-VA-2',
+        'network_name': 'Lab-test-spoke1',
         'VLANS': [
             {'id': 20, 'name': 'CDE', 'applianceIp': '172.16.27.1', 'subnet': '172.16.27.0/24', "dnsNameservers": "opendns"}
         ]
